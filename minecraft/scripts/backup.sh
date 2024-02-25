d8a275e25a698589cb6d24ff56b0e73e34255d39
ansanloms <ansanloms@users.noreply.github.com>
2024-02-25 13:48:46 +0900

    fix: script.

--- minecraft/scripts/backup.sh
+++ minecraft/scripts/backup.sh
@@ -4,10 +4,6 @@ set -euxo pipefail
 now=$(date +%Y%m%d%H%M)
 backupPath="/root/backup/world-${now}.tar.gz"
 
-mcrcon "say backup..." save-all
-
 cp -R /root/world /root/backup/.
 tar -zcvf ${backupPath} -C /root/backup world
 rm -rfv /root/backup/world
-
-mcrcon "say backup completed: ${backupPath}"
