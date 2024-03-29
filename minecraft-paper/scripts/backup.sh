0f9a62389fd2002a2ea8af1f81d9aea72dab56ab
ansanloms <ansanloms@users.noreply.github.com>
2024-03-30 05:29:02 +0900

    fix: backup script.

--- minecraft-paper/scripts/backup.sh
+++ minecraft-paper/scripts/backup.sh
@@ -5,5 +5,11 @@ now=$(date +%Y%m%d%H%M)
 backupPath="/root/backup/world-${now}.tar.gz"
 
 cp -R /root/world /root/backup/.
-tar -zcvf ${backupPath} -C /root/backup world
+cp -R /root/world_nether /root/backup/.
+cp -R /root/world_the_end /root/backup/.
+
+tar -zcvf ${backupPath} -C /root/backup world world_nether world_the_end
+
 rm -rfv /root/backup/world
+rm -rfv /root/backup/world_nether
+rm -rfv /root/backup/world_the_end
