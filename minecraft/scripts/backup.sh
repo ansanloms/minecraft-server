d1cbf0664e2dbd4bc517c576d4922a05717a0e0e
ansanloms <ansanloms@users.noreply.github.com>
2024-03-29 22:47:56 +0900

    fix: choose the launch version.

--- minecraft/scripts/backup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-
-now=$(date +%Y%m%d%H%M)
-backupPath="/root/backup/world-${now}.tar.gz"
-
-cp -R /root/world /root/backup/.
-tar -zcvf ${backupPath} -C /root/backup world
-rm -rfv /root/backup/world
